0dd8e33e4c799ba78a08c1cc6014fcb52ffb9146
sauszn <70582786+sauszn@users.noreply.github.com>
2020-10-24 15:30:07 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -78,7 +78,7 @@ def main():
     except:
         print("pass")
         pass
-for _ in range(3): 
+for _ in range(5): 
     randtime1 = random.randint(600,1200)
     time.sleep(randtime1)
     main()
